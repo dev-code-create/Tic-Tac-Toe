bb9c7fd5c9e63373704a3c004e577f8fc18b3281
dev-code-create <ayushkankale04@gmail.com>
2026-02-01 23:27:45 +0530

    Update gameController.js

--- server/Controller/gameController.js
+++ server/Controller/gameController.js
@@ -1,1 +1,24 @@
 // creating game logic controller
+const WINNING_COMBINATIONS = [
+  [0, 1, 2], // Top row
+  [3, 4, 5], // Middle row
+  [6, 7, 8], // Bottom row
+  [0, 3, 6], // Left column
+  [1, 4, 7], // Middle column
+  [2, 5, 8], // Right column
+  [0, 4, 8], // Diagonal \
+  [2, 4, 6], // Diagonal /
+];
+
+export const checkWinner = (board) => {
+  for (const combination of WINNING_COMBINATIONS) {
+    const [a, b, c] = combination;
+
+    if (board[a] && board[a] === board[b] && board[a] === board[c]) {
+      return {
+        winner: board[a],
+        line: combination,
+      };
+    }
+  }
+};
